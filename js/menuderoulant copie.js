52e7f553af943a9be3ce2d2835b1ddd6aebb52d7
Clélia Bianchi <clelia.bianchi@ynov.com>
2024-07-23 21:55:21 +0200

    portfolio avant article stage

--- js/menuderoulant copie.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("menu").addEventListener("click", function () {
-  if (this.classList.contains("toggle-menu--active")) {
-    this.classList.remove("toggle-menu--active");
-  } else {
-    this.classList.add("toggle-menu--active");
-  }
-});
